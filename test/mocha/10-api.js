453b49d184e84afedb342db08eb80aeb47732fa2
Matthew Collier <collier.matthew@gmail.com>
2017-01-08 14:53:08 -0500

    Add core files.

---
+++ test/mocha/10-api.js
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+const brJwt = require('bedrock-jwt-mongodb');
+
+describe('API', () => {
+  it('should work properly');
+});
