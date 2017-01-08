da48284254dedda6ec00182b52c2815bfc31b0c7
Matthew Collier <collier.matthew@gmail.com>
2017-01-08 14:53:08 -0500

    Add core files.

---
+++ test/test.config.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+const config = require('bedrock').config;
+const path = require('path');
+
+config.mocha.tests.push(path.join(__dirname, 'mocha'));
+
+// MongoDB
+config.mongodb.name = 'bedrock_jwt_mongodb_test';
+config.mongodb.host = 'localhost';
+config.mongodb.port = 27017;
+config.mongodb.local.collection = 'bedrock_jwt_mongodb_test';
+config.mongodb.dropCollections.onInit = true;
+config.mongodb.dropCollections.collections = [];
---
+++ test/test.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+const bedrock = require('bedrock');
+require('bedrock-jwt-mongodb');
+
+require('bedrock-test');
+bedrock.start();
